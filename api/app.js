6b51c516b58f9ddb628de3d389a8b2016d770d23
tanish <tanish@gmail.com>
2024-08-14 22:03:45 +0530

    Changed app.js backend

--- api/app.js
+++ api/app.js
@@ -1,70 +1,79 @@
 const express = require('express');
-const socket = require('socket.io');
-const http = require('http');
+const { Server } = require('socket.io');
 const { Chess } = require('chess.js');
 const path = require('path');
 
 const app = express();
-const server = http.createServer(app);
-const io = socket(server);
-const PORT = 3000;
-let chess = new Chess();
+const chess = new Chess();
 let players = {};
+let currentPlayer = "w";
 
+// Set view engine to EJS
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, "public")));
+app.set('views', path.join(__dirname, '../views'));
 
-app.get("/", (req, res) => {
-    res.render("index");
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Route for the homepage
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
-io.on('connection', (socket) => {
-    if (!players.white) {
-        players.white = socket.id;
-        socket.emit("playerRole", "w");
-    } else if (!players.black) {
-        players.black = socket.id;
-        socket.emit('playerRole', 'b');
-    } else {
-        socket.emit('spectatorRole');
-    }
+// Start the server and WebSocket
+const ioHandler = (req, res) => {
+    if (!res.socket.server.io) {
+        const server = res.socket.server;
+        const io = new Server(server);
 
-    socket.on('move', (move) => {
-        try {
-            if ((chess.turn() === 'w' && socket.id !== players.white) || (chess.turn() === 'b' && socket.id !== players.black)) {
-                return;
-            }
-
-            const result = chess.move(move);
-
-            if (result) {
-                io.emit('move', move);
-                io.emit('boardState', chess.fen());
-
-                if (chess.isCheckmate()) {
-                    const winner = chess.turn() === 'w' ? 'Black' : 'White';
-                    io.emit('gameOver', { winner });
-                }
+        io.on('connection', (socket) => {
+            if (!players.white) {
+                players.white = socket.id;
+                socket.emit("playerRole", "w");
+            } else if (!players.black) {
+                players.black = socket.id;
+                socket.emit('playerRole', 'b');
             } else {
-                socket.emit('invalidMove', move);
+                socket.emit('spectatorRole');
             }
-        } catch (err) {
-            console.log('Error while trying to move:', err);
-            socket.emit("errorInMoving", err);
-        }
-    });
 
-    socket.on('disconnect', () => {
-        console.log("Player disconnected");
+            socket.on('move', (move) => {
+                try {
+                    if (chess.turn() === 'w' && socket.id !== players.white) return;
+                    if (chess.turn() === 'b' && socket.id !== players.black) return;
+                    const result = chess.move(move);
 
-        if (socket.id === players.white || socket.id === players.black) {
-            chess = new Chess();
-            players = {};
-            io.emit('gameReset');
-        }
-    });
-});
+                    if (result) {
+                        currentPlayer = chess.turn();
+                        io.emit('move', move);
+                        io.emit('boardState', chess.fen());
+                    } else {
+                        socket.emit('InvalidMove', move);
+                    }
+                } catch (err) {
+                    socket.emit("ErrorInMoving", err);
+                }
+            });
 
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+            socket.on('disconnect', () => {
+                if (socket.id === players.white) {
+                    delete players.white;
+                } else if (socket.id === players.black) {
+                    delete players.black;
+                }
+
+                io.emit('gameReset');
+                chess.reset();
+                players = {};
+                currentPlayer = "w";
+            });
+        });
+
+        res.socket.server.io = io;
+    }
+    res.status(200).end();
+};
+
+app.all('/api/socket', ioHandler);
+
+module.exports = app;
